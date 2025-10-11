e0806909a66c1637ca42c5ed071820345243dd48
silvia <silviauzie77@gmail.com>
2025-10-11 16:00:53 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+/* Widgets are the building blocks of an APP's interface, they're responsible for 
+describing what the UI of an APP should be like  */
+import 'package:flutter/material.dart';
+
+void main() {
+  //runApp( MyApp);
+  runApp(app);
+}
