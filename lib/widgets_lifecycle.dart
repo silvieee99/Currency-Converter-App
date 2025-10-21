1986ce7bc336b9217b27d798331e13209064f4a5
silvia <silviauzie77@gmail.com>
2025-10-21 08:34:08 +0100

    Widget life cycle notes explanation

---
+++ lib/widgets_lifecycle.dart
@@ -0,0 +1,40 @@
+//SetState will help you re-run your build function again
+//for instance, if you wanna update a vlaue and show users on screen
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(covariant MyWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+//The create state is usually run first, before the initstate, did dependencies next,
+//did update widget runs next, then dispose before the build function runs
